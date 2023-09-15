72c1d49a580736220abea9416b7f22d8f7809bff
JamRafa <rafa.deandrade35@gmail.com>
2023-09-15 14:30:04 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
+import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
+
 // Rota GET para listar inscritos
-app.get('/inscritos', (req, res) => {
+app.get("/inscritos", (req, res) => {
   // Supondo que você tenha um arquivo "db.json" com os dados
-  const inscritos = require('./db.json').inscritos;
+  const inscritos = require("./db.json").inscritos;
   res.json(inscritos);
 });
 
 // Rota POST para criar um novo inscrito
-app.post('/inscritos', (req, res) => {
+app.post("/inscritos", (req, res) => {
   const newInscrito = req.body;
 
   // Supondo que você tenha um arquivo "db.json" com os dados
-  const db = require('./db.json');
+  const db = require("./db.json");
   db.inscritos.push(newInscrito);
 
   // Salvar a atualização no arquivo "db.json"
-  const fs = require('fs');
-  fs.writeFileSync('./db.json', JSON.stringify(db, null, 2));
+  const fs = require("fs");
+  fs.writeFileSync("./db.json", JSON.stringify(db, null, 2));
 
   res.json(newInscrito);
 });
