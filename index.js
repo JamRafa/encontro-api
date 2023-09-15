a314b75757139ddef3ed044d3a1d88b306b8f918
JamRafa <rafa.deandrade35@gmail.com>
2023-09-15 03:10:41 -0300

    export

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
+const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser); // Permite analisar solicitações POST com JSON no corpo
+app.use(middlewares);
+app.use(jsonServer.bodyParser); // Permite analisar solicitações POST com JSON no corpo
 
 // Rota GET personalizada para listar itens
-server.get("/inscritos", (req, res) => {
+app.get("/inscritos", (req, res) => {
   const inscritos = router.db.get("inscritos").value();
   res.json(inscritos);
 });
 
 // Rota POST personalizada para criar um novo item
-server.post("/inscritos", (req, res) => {
+app.post("/inscritos", (req, res) => {
   const newItem = req.body; // O corpo da solicitação deve conter o novo item
   router.db.get("inscritos").push(newItem).write();
   res.json(newItem);
 });
 
-server.use(router);
+app.use(router);
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`JSON Server está rodando na porta ${port}`);
 });
+
+module.exports = app
